d36c2c8bb25f16bf0c81f8a34cb2e4bfce0c94cd
g.ferrer <gustavo.ferrer@totvs.com.br>
2024-02-08 16:17:26 -0300

    fix: Adjusting layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gustavo Gotha - Software Engineer",
@@ -21,12 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased bg-black",
-          fontSans.variable
-        )}
-      >
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
